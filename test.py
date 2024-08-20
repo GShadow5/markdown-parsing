451a6348e1b8a35bf1ae6d3cbacb26c589c9f1a2
NSawyer <33187059+GShadow5@users.noreply.github.com>
2024-08-19 22:58:32 -0400

    added links and made it scan directories

--- test.py
+++ test.py
@@ -9,18 +9,19 @@ class TailwindExtension(Extension):
 class TailwindTreeprocessor(Treeprocessor):
 
     classes = {
-        'h1': ['text-3xl', 'font-bold', 'mt-4'],
-        'h2': ['text-2xl', 'font-bold', 'mt-4'],
-        'h3': ['text-2xl', 'font-bold', 'mt-4'],
-        'p': ['leading-relaxed'],
-        'ul': ['list-disc', 'list-inside', 'mt-4', 'leading-tight'],
-        'li': ['ml-4'],
+        'h1': "text-3xl font-bold mt-4",
+        'h2': "text-2xl font-bold mt-4",
+        'h3': "text-2xl font-bold mt-4",
+        'p': "leading-relaxed",
+        'ul': "list-disc list-inside ml-4 mt-4 leading-tight max-w-96",
+        'li': "my-2",
+        'a': "text-slate-700 underline hover:text-orange-500"
     }
     def run(self, root):
         for node in root.iter():
             tag_classes = self.classes.get(node.tag)
             if tag_classes:
-                node.attrib['class'] = ' '.join(tag_classes)
+                node.attrib['class'] = tag_classes
 
 
 
@@ -28,15 +29,25 @@ class TailwindTreeprocessor(Treeprocessor):
 
 
 
+import os
 
+for filename in os.listdir('.'):
+    if filename.endswith('.md'):
+        with open(filename, "r") as f:
+            markdownstring = f.read()
 
-with open("temp.md", "r") as f:
-    markdownstring = f.read()
+        htmlstring = markdown.markdown(markdownstring, extensions=[TailwindExtension()])
 
-htmlstring = markdown.markdown(markdownstring, extensions=[TailwindExtension()])
+        with open(os.path.splitext(filename)[0] + '.html', "w") as f:
+            f.write(htmlstring)
 
-with open("temp.html", "w") as f:
-    f.write(htmlstring)
+# with open("temp.md", "r") as f:
+#     markdownstring = f.read()
+
+# htmlstring = markdown.markdown(markdownstring, extensions=[TailwindExtension()])
+
+# with open("temp.html", "w") as f:
+#     f.write(htmlstring)
 
 
 
